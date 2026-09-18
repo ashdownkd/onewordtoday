dd0789d4dfbe726943d1ab013613eda828263650
Ashish <ashishyadav.work25@gmail.com>
2026-09-18 22:00:45 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -24,6 +24,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+    <meta name="google-adsense-account" content="ca-pub-2235714345131263">
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Fraunces:opsz,wght@9..144,300..700&family=Space+Grotesk:wght@400;500&display=swap"
