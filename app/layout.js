2e61b31a838ae85d89c7ae04aaf5d024d5ee0a98
Ashish <ashishyadav.work25@gmail.com>
2026-09-18 22:04:42 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -29,8 +29,6 @@ export default function RootLayout({ children }) {
           href="https://fonts.googleapis.com/css2?family=Fraunces:opsz,wght@9..144,300..700&family=Space+Grotesk:wght@400;500&display=swap"
           rel="stylesheet"
         />
-                <meta name="google-adsense-account" content="ca-pub-2235714345131263">
-
       </head>
       <body>{children}</body>
     </html>
